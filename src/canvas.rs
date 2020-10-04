8f4e10b5d25eb3085377719089a3d3857455091a
Константин Бедрицкий <kmb2@tpu.ru>
2020-10-04 16:49:27 +0600

    Canvas trait and SDL Canvas implementation to the trait

---
+++ src/canvas.rs
@@ -0,0 +1,41 @@
+use sdl2::{pixels::Color, rect::Point, video::Window};
+
+pub trait Canvas {
+    fn set_color(&mut self, color: &crate::Color);
+    fn draw_point(&mut self, x: u32, y: u32);
+    fn width(&self) -> u32;
+    fn height(&self) -> u32;
+    fn clear(&mut self);
+    fn flush(&mut self);
+}
+
+impl Canvas for sdl2::render::Canvas<Window> {
+    fn set_color(&mut self, color: &crate::Color) {
+        self.set_draw_color(Color::RGBA(
+            (color.r * 255.0) as u8,
+            (color.g * 255.0) as u8,
+            (color.b * 255.0) as u8,
+            (color.a * 255.0) as u8,
+        ));
+    }
+
+    fn draw_point(&mut self, x: u32, y: u32) {
+        self.draw_point(Point::new(x as i32, y as i32)).unwrap();
+    }
+
+    fn width(&self) -> u32 {
+        self.output_size().unwrap().0
+    }
+
+    fn height(&self) -> u32 {
+        self.output_size().unwrap().1
+    }
+
+    fn clear(&mut self) {
+        self.clear();
+    }
+
+    fn flush(&mut self) {
+        self.present();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,52 @@
+use canvas::Canvas;
 
+mod canvas;
 
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+impl Color {
+    pub fn new() -> Self {
+        Self {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+            a: 1.0,
+        }
+    }
+}
+
+pub struct Raytacer<'a> {
+    background: Color,
+    canvas: &'a mut dyn Canvas,
+}
+
+impl<'a> Raytacer<'a> {
+    pub fn new(canvas: &'a mut dyn Canvas) -> Raytacer {
+        Raytacer {
+            background: Color::new(),
+            canvas,
+        }
+    }
+
+    pub fn render(&mut self) {
+        self.canvas.set_color(&self.background);
+        self.canvas.clear();
+
+        let mut color = Color::new();
+        color.r = 0.5;
+        color.g = 0.3;
+        color.b = 0.2;
+
+        self.canvas.set_color(&color);
+        for i in 20..self.canvas.width() - 20 {
+            self.canvas.draw_point(i, 20);
+        }
+
+        self.canvas.flush();
     }
 }
